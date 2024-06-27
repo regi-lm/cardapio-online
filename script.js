f247c8f5c1e10397ae1d7b45cfbbbf82bd640271
regi-lm <djreginaldo3003@hotmail.com>
2024-06-27 18:47:17 -0300

    ajuste do horario de abertura/fechamento

--- script.js
+++ script.js
@@ -182,7 +182,7 @@ checkoutBtn.addEventListener("click", function() {
     // Enviar o pedido para a api do whatsapp
     const cartItems = cart.map((item) => {
         return (
-            `${item.name} Quantidade: (${item.quantity}) Preço: ${item.price} |`
+            `${item.name} Quantidade: (${item.quantity}) Preço: R$ ${item.price} | `
         )
     }).join("")
 
@@ -200,7 +200,7 @@ checkoutBtn.addEventListener("click", function() {
 function checkRestaurantOpen() {
     const data = new Date();
     const hora = data.getHours();
-    return hora >= 18 && hora < 0;
+    return hora >= 18 && hora < 24;
 }
 
 const spanItem = document.getElementById("date-span")
